78ef8a5e7a9a10dabd1fd57a97effff779010924
spider4216 <spider4216@gmail.com>
2017-04-30 22:14:51 +0600

    vnc feature

--- local_container/start.sh
+++ local_container/start.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-echo "Add host...  \n";
+echo "Add host...  \n"
 echo '127.0.0.1      test.loc' >> /etc/hosts
 
-echo "Starting services... \n";
+echo "Starting services... \n"
 service nginx start
 service postgresql start
 service php7.0-fpm start
-DISPLAY=:99 xvfb-run -a -n 1 -l -s "-screen 0, 1024x768x8" java -Dwebdriver.gecko.driver="/usr/local/src/geckodriver" -jar /usr/local/src/selenium-server-standalone-3.3.0.jar &
+
+export DISPLAY=:1
+Xvfb :1 -screen 0 1024x768x16 &
+fluxbox &
+java -Dwebdriver.gecko.driver="/usr/local/src/geckodriver" -jar /usr/local/src/selenium-server-standalone-3.3.0.jar &
+x11vnc -display :1 -bg -nopw -xkb
+
 ./yii migrate-app --migrationPath=@yii/rbac/migrations --interactive=0
-echo "ok \n"
+echo "VNC info:"
+echo ":0";
+hostname -I | cut -d' ' -f1
+
+sleep 20;
